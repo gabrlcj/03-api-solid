a3277130b79f45aab84660cf8a555bb0be875201
gabrlcj <gabrielbittencourt57@gmail.com>
2024-11-25 19:33:23 -0300

    Adds .env to project and validation with zod

---
+++ src/env/index.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import { z } from 'zod';
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  PORT: z.coerce.number().default(3333),
+});
+
+const tempEnv = envSchema.safeParse(process.env);
+
+if (tempEnv.success === false) {
+  console.error('Invalid environments variables', tempEnv.error.format());
+
+  throw new Error();
+}
+
+export const env = tempEnv.data;
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import { app } from './app';
+import { env } from './env';
 
 app
   .listen({
-    port: 3333,
+    host: '0.0.0.0',
+    port: env.PORT,
   })
   .then(() => {
     console.log(`Server is running on port: ${3333}`);
