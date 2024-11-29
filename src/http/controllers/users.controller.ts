83aa7fc79d7ddea30f5272ee0b847a39dfcadb89
gabrlcj <gabrielbittencourt57@gmail.com>
2024-11-29 18:47:53 -0300

    Adjusts project structure with controllers, services and routes to it

---
+++ src/http/controllers/users.controller.ts
@@ -0,0 +1,21 @@
+import { z } from 'zod';
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { createUserService } from '@/services/users.service';
+
+export async function createUser(request: FastifyRequest, reply: FastifyReply) {
+  const createUserBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
+
+  const { name, email, password } = createUserBodySchema.parse(request.body);
+
+  try {
+    createUserService({ name, email, password });
+  } catch (error) {
+    return reply.status(409).send();
+  }
+
+  return reply.status(201).send();
+}
