fa37ee8605a7288ccf67a903fd89549f45a0c345
gabrlcj <gabrielbittencourt57@gmail.com>
2025-01-27 18:51:05 -0300

    Adds prisma repository for check-ins

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,9 +1,9 @@
 import { prisma } from '@/lib/prisma';
-import { Prisma, User } from '@prisma/client';
+import { Prisma } from '@prisma/client';
 import { UsersRepository } from '../interfaces/users-repository';
 
 export class PrismaUsersRepository implements UsersRepository {
-  async create(data: Prisma.UserCreateInput): Promise<User> {
+  async create(data: Prisma.UserCreateInput) {
     const user = await prisma.user.create({
       data,
     });
@@ -11,7 +11,7 @@ export class PrismaUsersRepository implements UsersRepository {
     return user;
   }
 
-  async findByEmail(email: string): Promise<User | null> {
+  async findByEmail(email: string) {
     const userWithSameEmail = await prisma.user.findUnique({
       where: {
         email,
@@ -19,5 +19,15 @@ export class PrismaUsersRepository implements UsersRepository {
     });
 
     return userWithSameEmail;
+  }
+
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return user;
   }
 }
