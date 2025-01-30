db2ffe4c3796f51a36b000702479639ac7d43421
gabrlcj <gabrielbittencourt57@gmail.com>
2025-01-30 17:57:39 -0300

    Adds jwt to authenticate endpoint

--- src/http/controllers/authenticate.controller.ts
+++ src/http/controllers/authenticate.controller.ts
@@ -14,7 +14,20 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
   try {
     const authenticateSerivce = makeAuthenticateService();
 
-    await authenticateSerivce.execute({ email, password });
+    const { user } = await authenticateSerivce.execute({ email, password });
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      }
+    );
+
+    return reply.status(200).send({
+      token,
+    });
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: error.message });
@@ -22,6 +35,4 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
 
     throw error;
   }
-
-  return reply.status(200).send();
 }
--- src/http/controllers/profile.controller.ts
+++ src/http/controllers/profile.controller.ts
@@ -1,24 +1,9 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  // const authenticateUserBodySchema = z.object({
-  //   email: z.string().email(),
-  //   password: z.string().min(6),
-  // });
+  await request.jwtVerify();
 
-  // const { email, password } = authenticateUserBodySchema.parse(request.body);
-
-  // try {
-  //   const authenticateSerivce = makeAuthenticateService();
-
-  //   await authenticateSerivce.authenticateUser({ email, password });
-  // } catch (error) {
-  //   if (error instanceof InvalidCredentialsError) {
-  //     return reply.status(400).send({ message: error.message });
-  //   }
-
-  //   throw error;
-  // }
+  console.log(request.user.sub);
 
   return reply.status(200).send();
 }
